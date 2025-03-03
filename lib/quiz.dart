a71fb49c4db978f8151e760d5156b95641138034
國久 <>
2025-03-04 00:13:19 +0900

    画面遷移の方法

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,7 @@
+import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   Quiz({super.key});
@@ -10,9 +12,30 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  // Widget? activeScreen;
+  var activeScreen = 'start-screen';
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  void switchScreen() {
+    setState(() {
+      // activeScreen = QuestionsScreen();
+      activeScreen = 'questions-screen';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen != 'start-screen') {
+      screenWidget = QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -26,7 +49,7 @@ class _QuizState extends State<Quiz> {
               ],
             ),
           ),
-          child: StartScreen(),
+          child: screenWidget,
         ),
       ),
     );
