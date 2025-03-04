7b4f272663786ae47075a763379cab1568d89898
國久 <>
2025-03-05 00:10:23 +0900

    動的にボタンを表示するように追加

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-
   // AnswerButton(this.answerText, this.answerAction, {super.key});
   AnswerButton({
     super.key,
@@ -12,13 +11,23 @@ class AnswerButton extends StatelessWidget {
   final String answerText;
   void Function() answerAction;
 
-
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-            onPressed: answerAction,
-            style: ElevatedButton.styleFrom(),
-            child: Text(answerText),
-          );;
+      onPressed: answerAction,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(answerText),
+    );
+    ;
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,25 +14,41 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'The question...',
+            currentQuestion.text,
             style: TextStyle(
               color: Colors.white,
             ),
           ),
           SizedBox(height: 30),
-          AnswerButton(answerText: 'Answer 1', answerAction: () {}),
-          AnswerButton(answerText: 'Answer 2', answerAction: () {}),
-          AnswerButton(answerText: 'Answer 3', answerAction: () {}),
-          // AnswerButton('Answer 1', () {}),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, answerAction: () {});
+          }),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[0],
+          //   answerAction: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[1],
+          //   answerAction: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[2],
+          //   answerAction: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[3],
+          //   answerAction: () {},
+          // ),
+          // AnswerButton('Answer 1', () {})
           // AnswerButton('Answer 2', () {}),
           // AnswerButton('Answer 3', () {}),
-          
         ],
       ),
     );
