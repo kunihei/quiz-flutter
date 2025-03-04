1767000b4ed78b88c5ddd5c0b212ab0dd7956063
國久 <>
2025-03-05 00:12:38 +0900

    不要処理の削除

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,10 +1,9 @@
 import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
-import 'package:adv_basics/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
-  Quiz({super.key});
+  const Quiz({super.key});
   @override
   State<StatefulWidget> createState() {
     return _QuizState();
